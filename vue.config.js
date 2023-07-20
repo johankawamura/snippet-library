4439606aba34731c312512988c00b137f84652b5
johankawamura <accounts@johankawamura.com>
2023-07-20 15:35:28 +0900

    add auto deploy

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/template-vue/" : "/",
   css: {
     loaderOptions: {
       sass: {
